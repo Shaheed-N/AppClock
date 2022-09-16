31459b5b5ee5f8bb3e1ef813cb8821878e5aa033
Shaheed-N <username@example.com>
2022-09-16 13:33:00 +0400

    Initial Commit

---
+++ AppClock/AppClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppClockApp.swift
+//  AppClock
+//
+//  Created by Shahid on 16.09.22.
+//
+
+import SwiftUI
+
+@main
+struct AppClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
